e49caacae4b10365abfd02aa96969c098e928869
Ruslan <94397861+Nikolrusik@users.noreply.github.com>
2022-11-18 23:03:14 +0300

    Lesson 2 (#2)
    * Create project and apps. Create base API for apps users, authors
    
    * Delete authors, added frontend on React and components
    
    * Added request to backend, added render response
    
    * Add footer and menu

--- users/serializers.py
+++ users/serializers.py
@@ -5,4 +5,5 @@ from .models import User
 class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = ['username', 'first_name', 'last_name', 'email', 'age']
+
