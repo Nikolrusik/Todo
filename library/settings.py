2c6ca8d34c48f188189736b86e92104cb02144b5
unknown <nikolrusik@gmail.com>
2022-11-21 21:28:13 +0300

    Added base pagination(test), added CustomViewSet to UserModel

--- library/settings.py
+++ library/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    # 'django-filter',
+    'django_filters',
     'users',
     'todo',
     'corsheaders'
@@ -137,11 +137,12 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
 ]
 
-
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1,
 }
-
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
+
 from .models import Project, Todo
 from .serializers import ProjectModelSerializer, TodoModelSerializer
+
+
+class TodoLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 1
 
 
 class ProjectModelViewSet(ModelViewSet):
@@ -11,3 +17,5 @@ class ProjectModelViewSet(ModelViewSet):
 class TodoModelViewSet(ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoModelSerializer
+    filterset_fields = ['user']
+    pagination_class = TodoLimitOffsetPagination
