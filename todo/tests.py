674976d54ed9e432e4b4e93f1faa41cbfee6f964
unknown <nikolrusik@gmail.com>
2022-12-20 22:45:55 +0300

    added tests on APIClient

--- todo/tests.py
+++ todo/tests.py
@@ -5,6 +5,7 @@ from rest_framework.test import APIRequestFactory, force_authenticate,APIClient,
 # from mixer.backend.django import mixer
 from users.models import User 
 from .views import ProjectModelViewSet
+from .models import Project
 
 # 1. APIRequestFactory.
 class TestProjectsViewSet(TestCase):
@@ -35,5 +36,35 @@ class TestProjectsViewSet(TestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 # 2. APIClient.
+    def test_get_detail(self):
+        ### Test check get-request for detail page
+        user = User.objects.create(username='user', email='user@user.ru', password='user12345')
+        project = Project.objects.create(name='Project 1')
+        project.users.set([user])
+        client = APIClient()
+        response = client.get(f'/api/projects/{project.id}/')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+    
+    def test_edit_quest(self):
+        ### Test check put-request for unauthorized user
+        user = User.objects.create(username='user', email='user@user.ru', password='user12345')
+        project = Project.objects.create(name='Project 1')
+        project.users.set([user])
+        client = APIClient()
+        response = client.put(f'/api/projects/{project.id}/', {'name': 'Project 2'})
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+    def test_edit_admin(self):
+        ### Test check put-request for admin
+        admin = User.objects.create_superuser(username='admin', email='admin@admin.ru', password='admin12345')
+        client = APIClient()
+        client.login(username='admin', password='admin12345')
+        project = Project.objects.create(name='Project 1')
+        project.users.set([admin])
+        response = client.put(f'/api/projects/{project.id}/', {'name':'Project 2', 'users': ["http://localhost:8000/api/users/1/"]}, format='json')
+        project = Project.objects.get(id=project.id)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(project.name, 'Project 2')
+        client.logout()
+        
 # 3. APITestCase.
